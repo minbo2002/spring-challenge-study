2af51e3c424476160dc435db42a3a93cb69e9b0d
minbo2002 <minbo2002@naver.com>
2023-10-19 22:13:12 +0900

    feat: 1주차 환경설정

---
+++ 1주차/src/main/java/com/example/springchallenge1week/SpringChallenge1weekApplication.java
@@ -0,0 +1,13 @@
+package com.example.springchallenge1week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChallenge1weekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChallenge1weekApplication.class, args);
+    }
+
+}
