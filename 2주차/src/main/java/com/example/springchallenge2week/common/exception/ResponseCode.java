b91b563fa9bda62b28a75b4eb9333b6f95a0ad93
minbo2002 <minbo2002@naver.com>
2023-10-27 14:43:08 +0900

    chore: 커스텀 에러코드 추가

--- 2주차/src/main/java/com/example/springchallenge2week/common/exception/ResponseCode.java
+++ 2주차/src/main/java/com/example/springchallenge2week/common/exception/ResponseCode.java
@@ -14,10 +14,12 @@ public enum ResponseCode {
     FORBIDDEN_CODE(403, "권한이 없습니다."),
 
     INVALID_REQUEST(400, "잘못된 요청입니다."),
-    NO_TARGET(404, "해당되는 대상이 없습니다."),
     INVALID_DISCOUNT(400, "할인쿠폰이 아닙니다."),
+    PRIVATE_COUPON(400, "비활성화된 쿠폰입니다."),
+    NO_TARGET(404, "해당되는 대상이 없습니다."),
     NO_TARGET_COUPON(404, "해당하는 쿠폰이 없습니다."),
     DUPLICATED_ID(422, "Id가 중복되어 있습니다."),
+    ALREADY_ISSUED_COUPON(422, "이미 발급된 쿠폰입니다."),
     USERNAME_ALREADY_EXISTS(422, "이미 존재하는 회원입니다."),
     AWS_S3_UPLOAD_ERROR(422, "AWS S3 업로드에 실패했습니다."),
     IO_EXCEPTION_CODE(422, "IO Exception이 발생했습니다."),
